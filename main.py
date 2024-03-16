9303de945280c8a5519c995c0a26d73957c43741
monique1291 <91268958+monique1291@users.noreply.github.com>
2024-03-16 11:36:38 -0500

    Grammar updated to only accept identifiers which begin with a capital letter. This should be working with the letters

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from token import NUMBER
-from ply import lex
-import keyword
-
+import ply.lex as lex
 import tokens
+
 lexer = lex.lex(module=tokens)
 data = '''
 3 + 4
 '''
-lexer.input("3+4 Moni Que")
+lexer.input("M = ( 3 * 6) - Y + (6 / 2) , moni 9.9")
 
 for tok in lexer:
     print(tok)
+
